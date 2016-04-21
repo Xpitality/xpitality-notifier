4a4d839a70918f9dfb3ac5ce1ccb59674dad6e98
Marko Bošković <bosskovic@gmail.com>
2016-04-21 17:27:20 +0200

    removes stub spec, adds semaphore badge

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://semaphoreci.com/api/v1/marko_xpitality/xpitality-notifier/branches/master/badge.svg)](https://semaphoreci.com/marko_xpitality/xpitality-notifier)
+
 # Xpitality::Notifier
 
 
--- spec/xpitality/notifier_spec.rb
+++ spec/xpitality/notifier_spec.rb
@@ -4,8 +4,4 @@ describe Xpitality::Notifier do
   it 'has a version number' do
     expect(Xpitality::Notifier::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
