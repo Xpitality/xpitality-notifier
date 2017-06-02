cbd5e16f515624d75f024d0911de676b04d7ee99
Marko Bošković <bosskovic@gmail.com>
2017-06-02 16:59:35 +0200

    adds support for creating custom bugsnag tabs

--- lib/xpitality/notifier/exception/bugsnag_notifier.rb
+++ lib/xpitality/notifier/exception/bugsnag_notifier.rb
@@ -4,7 +4,13 @@ module Xpitality
       class BugsnagNotifier
 
         def self.notify(message, options)
-          Bugsnag.notify message
+          Bugsnag.notify(message) do |notification|
+            if options[:tabs]
+              options[:tabs].each do |key, value|
+                notification.add_tab(key.to_sym, value)
+              end
+            end
+          end
         end
       end
     end
